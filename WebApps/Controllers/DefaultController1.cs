3473c2616d4e7b2fee96185640ae43511643d289
Onelife <ortcellad@gmail.com>
2021-01-01 23:23:00 +0300

    Partial Examples

---
+++ WebApps/Controllers/DefaultController1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApps.Controllers
+{
+    public class DefaultController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detaylar(Models.Urun detay)
+        {
+            var bilgi = new Models.Urun();
+            bilgi.adi = "Bilgisayar";
+            bilgi.aciklamasi = "5 inc";
+            bilgi.fiyat = 7500;
+            bilgi.turu = "Asus";
+            bilgi.durum = true;
+
+            return View(bilgi);
+        }
+    }
+}
---
+++ WebApps/Models/Urun.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApps.Models
+{
+    public class Urun
+    {
+        public string adi { get; set; }
+        public string turu { get; set; }
+        public string aciklamasi { get; set; }
+        public int fiyat { get; set; }
+        public bool durum { get; set; }
+        public string durum2 { get 
+            { 
+            
+            if(durum==true)
+                {
+                    return "Sıfır";
+                }
+            else
+                {
+                    return "İkinci El";
+                }
+            
+            
+            }}
+    }
+}
